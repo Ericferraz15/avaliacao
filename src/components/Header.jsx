83d5bcd071c094572eb14db9602885441337f101
GabrielAtunes <g.antunes2002@gmail.com>
2025-04-23 12:51:52 -0300

    Altera o nome da aplicação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-﻿export default function Header({ title = "Shopfloor Daily Control" }) {
+﻿export default function Header({ title = "Day@Shop Manager" }) {
   return (
-    <header className="bg-white shadow-sm p-4 border-b flex justify-between items-center ml-16"> {/* Adicionei ml-16 */}
+    <header className="bg-white shadow-sm p-4 border-b flex justify-between items-center ml-16">
+      {" "}
+      {/* Adicionei ml-16 */}
       <h1 className="text-xl font-semibold text-valmet-blue">{title}</h1>
       <div className="flex items-center space-x-4">
         <span className="text-valmet-dark">Usuário</span>
